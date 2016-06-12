7ad3ac7695b37388f38286b955532c284e293b2e
Brian Guo <Brian@brians-macbook.home>
2016-06-12 15:22:49 -0500

    fixed database settings hopefully

--- taskbuster/settings/production.py
+++ taskbuster/settings/production.py
@@ -8,16 +8,16 @@ TEMPLATES[0]['OPTIONS']['debug'] = DEBUG
 SITE_ID = 3
 ALLOWED_HOSTS_ = ['brianstaskbuster.herokuapp.com']
 SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
-DATABASES['default'] = dj_database_url.config(conn_max_age=500)
-"""DATABASES = {
-    'default' : {
+#DATABASES['default'] = dj_database_url.config(conn_max_age=500)
+DATABASES = {
+    'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'taskbuster_db',
-        'USER': 'myusername',
-        'PASSWORD': 'mypassword',
+        'NAME': get_env_variable('DATABASE_NAME'),
+        'USER': get_env_variable('DATABASE_USER'),
+        'PASSWORD': get_env_variable('DATABASE_PASSWORD'),
         'HOST': '',
         'PORT': '',
-        }
-    }"""
+    }
+}
 
 #DATABASES['default'] = dj_database_url.config()
